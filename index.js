bcf6d479230d44ffb62cd00aefe5ffb5c1e4c311
NathanB09 <nathanbell09@gmail.com>
2019-01-08 10:18:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = (collection) => {return collection.slice(0, 2)}
+const returnFirstTwoDrivers = (collection) => collection.slice(0, 2)
 
-const returnLastTwoDrivers = (collection) => {return collection.slice(-2)}
+const returnLastTwoDrivers = (collection) => collection.slice(-2)
 
 const selectingDrivers = [
   returnFirstTwoDrivers,
   returnLastTwoDrivers
 ]
 
-const createFareMultiplier = (mult) => {return (num) => num * mult}
+const createFareMultiplier = (mult) => (num) => num * mult
 
-const fareDoubler = (num) => {return num * 2}
+const fareDoubler = (num) => num * 2
 
-const fareTripler = (num) => {return num * 3}
+const fareTripler = (num) => num * 3
 
-const fetchSpecifiedDrivers = (array, fn) => {return fn(array)}
+const fetchSpecifiedDrivers = (array, fn) => fn(array)
