e3c236d8878d1c879f7071335cbc3d51de462458
morwen80 <ribesnero80@gmail.com>
2019-01-09 13:05:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// const twoDrivers = (function returnFirstTwoDrivers(drivers) {
-//   return drivers.slice(1,3);
-// });
-
 const returnFirstTwoDrivers = function(drivers) {
   return drivers.slice(0,2);
 };
@@ -10,17 +6,13 @@ const returnLastTwoDrivers = function(drivers) {
   return drivers.slice(-2);
 };
 
-const selectingDrivers = [
-  returnFirstTwoDrivers,
-  returnLastTwoDrivers
-]
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ]
 
 const createFareMultiplier = function(num){
   return function(fare){
     return fare * num
   }
 }
-
 
 const fareDoubler = createFareMultiplier(2);
 
